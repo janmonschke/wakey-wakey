e93dfebca05a9f810d2c45ffd7e9002ebcbe7fa7
Jan Monschke <janmonschke@fastmail.com>
2025-04-04 20:17:33 +0200

    overwrite window

--- src/vendor/puck.js
+++ src/vendor/puck.js
@@ -60,18 +60,19 @@ ChangeLog:
 
 */
 (function (root, factory) {
-  if (typeof define === "function" && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], factory);
-  } else if (typeof module === "object" && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
-    module.exports = factory();
-  } else {
-    // Browser globals (root is window)
-    root.Puck = factory();
-  }
+  window.Puck = factory();
+  // if (typeof define === "function" && define.amd) {
+  //   // AMD. Register as an anonymous module.
+  //   define([], factory);
+  // } else if (typeof module === "object" && module.exports) {
+  //   // Node. Does not work with strict CommonJS, but
+  //   // only CommonJS-like environments that support module.exports,
+  //   // like Node.
+  //   module.exports = factory();
+  // } else {
+  //   // Browser globals (root is window)
+  //   root.Puck = factory();
+  // }
 })(typeof self !== "undefined" ? self : this, function () {
   if (typeof navigator == "undefined") return;
 
